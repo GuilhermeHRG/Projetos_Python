6b0bc82a4436cef8d8bb61ed01b35bdddd8c0d84
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-08-12 20:56:28 -0300

    Aponta a área de um quadrado
    e também a área ao quadrado

---
+++ Area_quadrado.py
@@ -0,0 +1,8 @@
+print("area do quadrado")
+for i in range(0,4):
+    a=int(input("Digite o tamanho de um dos lados: "))
+    area=a*a
+
+print(f'a área é de {area}')
+arq=area*area
+print(f'a área ao quadrado é {arq}')
