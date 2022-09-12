6febd249263bfb4d9a963b08824920c6924c1d3b
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-09-12 20:58:15 -0300

    Update Celsius.py

--- Celsius.py
+++ Celsius.py
@@ -2,4 +2,4 @@
 Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
 """
 c=float(input("digite a temperatura em Celsisus: "))
-print(f'a temperatura de {c} graus Celsius em Fahrenheit fica {c*1.8+32}')
+print(f'a temperatura de {c} graus Celsius em Fahrenheit fica {round(c*1.8+32,3)}')
