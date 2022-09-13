cf921200370020bde5fd1a32dd8f8f1e0a011bbf
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-09-12 21:40:08 -0300

    Update lista_compras.py

--- lista_compras.py
+++ lista_compras.py
@@ -1,13 +1,14 @@
 """--LISTA DE COMPRAS--"""
 carrinho = []
 produto = ''
-while produto != 'sair':
+while produto != 'sair' and produto != 'SAIR':
     print("digite o produto desejado ou 'sair' para sair: ")
     produto = input()
-    if produto != 'sair':
+    if produto != 'sair' and produto != 'SAIR':
         carrinho.append(produto)
+        carrinho.sort()
 print('lista de compras')
 print('\nindice', ' ', 'produto')
 for indice, produto in enumerate(carrinho):
     print('----------------------')
-    print(indice, '       ', produto)
+    print(indice+1, '       ', produto)
