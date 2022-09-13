efe6bbdeed84c4f1940f91ec2e329b6c7e4ebb34
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-09-12 21:51:01 -0300

    Update lista_compras.py

--- lista_compras.py
+++ lista_compras.py
@@ -1,14 +1,23 @@
 """--LISTA DE COMPRAS--"""
-carrinho = []
-produto = ''
+carrinho = [] # LISTA VAZIA
+produto = None #VARIÁVEL PRODUTO SE INICIA COM NONE
+
+#LOOP INICIADO COM REGRAS DE NEGÓCIO
 while produto != 'sair' and produto != 'SAIR':
-    print("digite o produto desejado ou 'sair' para sair: ")
+    print("\n\ndigite o produto desejado ou 'sair' para sair: ")
     produto = input()
+    # REGRA DE NEGÓCIO NO IF 
     if produto != 'sair' and produto != 'SAIR':
         carrinho.append(produto)
         carrinho.sort()
-print('lista de compras')
-print('\nindice', ' ', 'produto')
+    # SAÍDA DE DADOS 
+print('\nLISTA DE COMPRA')
+print('\nINDICES', ' ', 'PRODUTOS')
+
+#LOOP COM DADOS QUE ESTÃO NA LISTA
 for indice, produto in enumerate(carrinho):
     print('----------------------')
+    
+    #SAÍDA DE DADOS 
     print(indice+1, '       ', produto)
+#ESTE +1 AQUI   ^ É PARA QUE O ÍNDICE INICIE COM 1
