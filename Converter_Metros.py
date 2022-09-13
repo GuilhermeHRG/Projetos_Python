f19fae6cf99e30a90a62e708f3e507252f7aaf20
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-09-12 21:02:36 -0300

    Update Converter_Metros.py

--- Converter_Metros.py
+++ Converter_Metros.py
@@ -1,6 +1,11 @@
 """
 Faça um Programa que converta metros para centímetros.
 """
+# ENTRADA DE DADOS DO USUÁRIO
 mt=float(input("Digite um valor em Metros: "))
+
+# AQUI A VARIÁVEL CT (CENTIMETROS) RECEBE O CÁLCULO DE CONVERSÃO METROS / CENTÍMETROS
 ct=mt*100
-print(f'\nO valor de {mt} metros em cm fica :{ct}cm.')
+
+# SAÍDA DE DADOS
+print(f'\nO valor de {mt} metros em cm fica :{ct} cm.')
