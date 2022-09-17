235a8255a6fe1f96cece358847b40531716c4ed0
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-09-17 15:36:55 -0300

    Update Converter_graus.py

--- Converter_graus.py
+++ Converter_graus.py
@@ -1,4 +1,9 @@
+# ENTRADA DE DADOS FORNECIDA PELO USUÁRIO
 print("temperatura")
 grau=int(input('Digite a temperatura em graus Fahrenheit: '))
+
+# VARIÁVEL ARMAZENANDO O CÁLCULO COM BASE NA FÓRMULA DE CONVERSÃO
 c= 5*((grau-32)/9)
+
+# SAÍDA DE DADOS COM FORMATAÇÃO PARA MOSTRAR O CONTEÚDO DAS VARIÁVEIS ENTRE CHAVES
 print(f'\na temperatura de {grau} Fahrenheit em Celsius é {c}')
