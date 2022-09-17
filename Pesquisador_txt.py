291bf5a9f4084b515a1c335b6d870fd6a4c1d6c9
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-09-17 16:32:10 -0300

    Update Pesquisador_txt.py

--- Pesquisador_txt.py
+++ Pesquisador_txt.py
@@ -1,5 +1,12 @@
-txt=input("digite ou cole um texto base aqui >> ")
+# ENTRADA DE DADOS
+txt = input("digite ou cole um texto base aqui >> ")
+
+# SAÍDA DE DADOS COM FORMATAÇÃO E FUNÇÃO LEN PARA CALCULAR TAMANHO
 print(f'o texto tem {len(txt.strip())} caracteres contando os espaços')
 print(f'texto base: {txt}')
+
+# VARIÁVEL ARMAZENANDO O QUE O USUÁRIO DIGITAR
 substring = input('\no que deseja procurar? ')
+
+# SAÍDA DE DADOS COM FORMATAÇÃO E FUNÇÃO COUNT PARA CALCULAR QUANTIDADES
 print(f'temos {txt.lower().count(substring)}  "{substring}"  no texto  ')
